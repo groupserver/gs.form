5b021429c93758d6240ae1bc4568bb47274b237d
Michael JasonSmith <mpj17@onlinegroups.net>
2012-07-16 11:25:13 +1000

    Removing all the old gs.content.form modules

--- gs/content/form/select.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from zope.app.form.browser import SelectWidget
-
-def select_widget(field, request):
-    retval = SelectWidget(field, field.vocabulary, request)
-    retval.size = 15 # Because there are a lot of items.
-    return retval
-
--- gs/content/form/wymeditor.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from zope.app.form.browser import TextAreaWidget
-
-def wym_editor_widget(field, request):
-    retval = TextAreaWidget(field, request)
-    retval.cssClass = 'wymeditor'
-    return retval
-
