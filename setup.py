9dec8e40388a0dae4ad537273f6cc7ef4417c863
Michael JasonSmith <mpj17@onlinegroups.net>
2013-08-27 10:47:37 -0700

    Updating the copyrightm license, and egg metadata.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -11,30 +11,28 @@ setup(name='gs.form',
     long_description=open("README.txt").read() + "\n" +
                       open(os.path.join("docs", "HISTORY.txt")).read(),
     classifiers=[
-      "Development Status :: 4 - Beta",
-      "Environment :: Web Environment",
-      "Framework :: Zope2",
-      "Intended Audience :: Developers",
-      "License :: Other/Proprietary License",
-      "Natural Language :: English",
-      "Operating System :: POSIX :: Linux"
-      "Programming Language :: Python",
-      "Topic :: Software Development :: Libraries :: Python Modules",
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
       ],
-    keywords='zope form formlib zope.formlib',
+    keywords='zope form formlib zope.formlib post',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='http://onlinegroups.net/',
-    license='other',
+    url='http://groupserver.org',
+    license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs'],
     include_package_data=True,
     zip_safe=True,
     install_requires=[
-        'setuptools',
-        # -*- Extra requirements: -*-
+        'setuptools',  # --=mpj17=-- Actually the complete set of egg deps
     ],
     entry_points="""
     # -*- Entry points: -*-
     """,)
-
